acd2c85c5d69b589de2b41df2d95b66e1c47c0d9
marc-fayad <marc.v.fayad@gmail.com>
2023-04-21 21:04:18 -0500

    Revised account file

--- account.py
+++ account.py
@@ -1,18 +1,9 @@
 class Account:
-    def __init__(self, name: str) -> None:
-        """
-        Function to set up account object.
-        :param name: Account name
-        """
+    def __init__(self, name):
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount) -> bool:
-        """
-        Function to deposit money to account.
-        :param amount: Amount of money to add
-        :return: Boolean value based on success of deposit
-        """
+    def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
             return True
@@ -21,12 +12,7 @@ class Account:
             return False
 
     def withdraw(self, amount):
-        """
-        Function to withdraw money from account.
-        :param amount: Amount of money to remove
-        :return: Boolean value based on success of withdrawal
-        """
-        if 0 < amount <= self.__account_balance:
+        if amount > 0 and amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
 
@@ -34,15 +20,7 @@ class Account:
             return False
 
     def get_balance(self):
-        """
-        Function to return account balance.
-        :return: Amount of money in account
-        """
         return self.__account_balance
 
     def get_name(self):
-        """
-        Function to return account name.
-        :return: Name of account
-        """
         return self.__account_name
